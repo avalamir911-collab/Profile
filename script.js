9372a5b92785a992e9fb265139b8e10edaf38ec6
avalamir911-collab <avalamir911@gmail.com>
2025-09-14 12:52:59 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+const sphere = document.querySelector('.sphere');
+let targetX = 0, targetY = 0;
+let currentX = 5, currentY = 5; 
+
+document.addEventListener('mousemove', e => {
+  targetX = (window.innerWidth / 2 - e.clientX) / 20;
+  targetY = (window.innerHeight / 2 - e.clientY) / 20;
+});
+
+function animateSphere() {
+  currentX += (targetX - currentX) * 0.1;
+  currentY += (targetY - currentY) * 0.1;
+  sphere.style.transform = `rotateX(${currentY}deg) rotateY(${currentX}deg)`;
+  requestAnimationFrame(animateSphere);
+}
+animateSphere();
+
+const canvas = document.getElementById('particle-canvas');
+const ctx = canvas.getContext('2d');
+let particles = [];
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.setTransform(1,0,0,1,0,0);
+}
+window.addEventListener('resize', () => {
+  resizeCanvas();
+  initParticles(); 
+});
+
+class Particle {
+  constructor(x, y, size, color, speedX, speedY) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.speedX = speedX;
+    this.speedY = speedY;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+  }
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particles = [];
+  let count = window.innerWidth < 768 ? 30 : 100;
+  for (let i = 0; i < count; i++) {
+    let size = Math.random() * 3 + 1;
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    let speedX = (Math.random() - 0.5) * 1;
+    let speedY = (Math.random() - 0.5) * 1;
+    particles.push(new Particle(x, y, size, 'rgba(0,240,255,0.7)', speedX, speedY));
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(p => { p.update(); p.draw(); });
+  requestAnimationFrame(animateParticles);
+}
+
+resizeCanvas();
+initParticles();
+animateParticles();
